6f26e90741ced8315f4c18bb95d8aadda13c7d0b
Dhruv Tikhande <itcs95123@gmail.com>
2026-04-03 07:36:15 +0000

    >..

--- api/predict.py
+++ api/predict.py
@@ -5,20 +5,13 @@ model = pickle.load(open("stress_model.pkl", "rb"))
 vectorizer = pickle.load(open("tfidf_vectorizer.pkl", "rb"))
 
 def handler(request):
-    try:
-        body = json.loads(request.body)
-        text = body.get("text", "")
+    body = json.loads(request.body)
+    text = body.get("text", "")
 
-        transformed = vectorizer.transform([text])
-        prediction = model.predict(transformed)[0]
+    transformed = vectorizer.transform([text])
+    prediction = model.predict(transformed)[0]
 
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"prediction": int(prediction)})
-        }
-
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"error": str(e)})
-        }
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"prediction": int(prediction)})
+    }
